bfb3331753b48e138da87fdbb4950680c2f7638c
bartul_9 <bartul123@outlook.com>
2020-12-30 19:14:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 "useStrict";
 
+//Buttons and other classes
+
 const btnOne = document.querySelector(".optionOne");
 const btnTwo = document.querySelector(".optionTwo");
-const btnNew = document.querySelector(".newGame");
-let paragraf = document.querySelector(".paragrafOne");
+const paragraf = document.querySelector(".paragrafOne");
+const gameScreen = document.querySelector(".One");
+
+//Images
 
 let url = "https://images5.alphacoders.com/102/1022372.jpg";
 
@@ -19,56 +23,160 @@ let url4 =
 let url5 = "https://i.ytimg.com/vi/VFNAJC7H96M/maxresdefault.jpg";
 
 let url6 =
-  "https://i1.wp.com/www.toiletovhell.com/wp-content/uploads/2015/04/bloodborne-youdiedlol2.png?ssl=1";
+  "https://i.pinimg.com/originals/c9/48/68/c94868267611bc1fde76574be34a6610.jpg";
+
+let url7 = "https://pbs.twimg.com/media/DdxrD3DXkAEC3ZJ.jpg";
+
+let url8 =
+  "https://www.onlineunitedstatescasinos.com/wp-content/uploads/2020/04/Cyberpunk-City-Online-Slot-Intro-Game.jpg";
+
+let url9 =
+  "https://cdna.artstation.com/p/assets/images/images/020/058/164/large/stanislav-kemezh-screen-1.jpg?1566197986";
+
+let funeral = "https://i.ytimg.com/vi/YOHlVCRNwyU/maxresdefault.jpg";
 
 let newimg = "https://i.redd.it/8wk4f2yryqey.jpg";
 
-let div = document.querySelector(".One");
+let gamble =
+  "https://geeks.bet/wp-content/uploads/Cyberpunk-2077-Multiplayer-Mode.jpg";
+
+//Character stats
+const cyberHunter = { stamina: 10, strenght: 15, health: 25 };
+const cyberMage = { stamina: 15, strenght: 10, health: 30 };
+
+//playing mechanic
+let playing = true;
+
+//Function for changing inner text and image
+
+const imgTxtChange = function (img, text, color, size = "30px") {
+  paragraf.innerText = text;
+  gameScreen.style.backgroundImage = `url(${img})`;
+  paragraf.style.textAlign = "center";
+  paragraf.style.fontSize = size;
+  paragraf.style.color = color;
+  paragraf.style.fontWeight = 700;
+  //style.backgroundImage = `url(${url})`;
+};
+
+// Function for rolling die
+const rollDie = function () {
+  return Math.floor(Math.random() * 25) + 1;
+};
+let randomNumber = rollDie();
+
+//Function for reseting the game
+
+const resetGame = function () {
+  paragraf.innerText =
+    "You go to No Tell motel in search for your girlfrind who works there.You enter a room. You see a guy dressed in chicken in front of you,there is blood all over the room. You pull out your knife. Do you want to fight or run?";
+  gameScreen.style.backgroundImage = `url(${newimg})`;
+  paragraf.style.fontSize = "30px";
+};
+
+//************************************************************************************/
+
+//If mage was selected
 
 btnOne.addEventListener("click", function () {
-  paragraf.style.backgroundColor = `rgba(164, 193, 218, 0.3)`;
-  paragraf.style.color = "yellow";
-  paragraf.style.fontWeight = "700";
-  paragraf.textContent =
-    "You choose to fight. You fought, bravely and won. The guy had an axe and hammer in his hands, but he could not beat you. You are a strong one, thats for shure. You have blood all over you, and you are hurt. You go out, you see three more coming towards you, you will?";
-  btnOne.textContent = "Hide";
-  btnTwo.textContent = "Run";
-  div.style.backgroundImage = `url(${url})`;
-  btnTwo.addEventListener("click", function () {
-    document.body.style.border = "5px solid red";
-    document.body.style.backgroundImage = `url(${url6})`;
-    btnOne.textContent = "Game Over";
-    btnTwo.textContent = "Game Over";
-    div.style.backgroundImage = `url(${url5})`;
-    paragraf.textContent =
-      "You start running and you come to random people, they all look at you strangely and avoid you. You cry for help and finnaly a women comes to you, she gives you her hand, and when you grab her hand she pulls out her gun, and puts a bullet right between your eyebrows. - Refresh page to start again";
-  });
-
-  btnOne.addEventListener("click", function () {
-    paragraf.textContent =
-      "You hidden in a trash near you. They look around but they didnt found you. You are lucky.You exit trash container you were in. Now you are dirty and you are bleeding. You see a phone booth near, you go there and try to call Police. No one answers, the phone is not working.";
-    btnOne.textContent = "Go to other phone booth";
-    btnTwo.textContent = "Run towards the crowd";
-    div.style.backgroundImage = `url(${url3})`;
-    btnOne.addEventListener("click", function () {
-      paragraf.textContent =
-        "You got to other phone booth, but on the way there the guys that were looking for you found you, and kill you. You are dead. - Refresh page to start again";
-      document.body.style.border = "5px solid red";
-      btnOne.textContent = "Game Over";
-      btnTwo.textContent = "Game Over";
-      div.style.backgroundImage = `url(${url4})`;
-    });
-  });
+  btnOne.innerText = "Next";
+  imgTxtChange(
+    url8,
+    "You have choosen the CyberHunter. Well, all i gotta say is good luck with hunting sir.",
+    "blue"
+  );
+  btnTwo.classList.add("noneDisplay");
+  //Calling the first room function so next slide can open
+  btnOne.addEventListener("click", firstVictim);
 });
 
-btnTwo.addEventListener("click", function () {
-  paragraf.style.backgroundColor = `rgba(164, 193, 218, 0.85)`;
-  paragraf.style.color = "black";
-  paragraf.style.fontWeight = "700";
-  paragraf.textContent =
-    "You choose to run like a pussy. You even pulled out your knife, but he managed to catch you in the end and kill you with his axe, you are a dead man now, my son. - Refresh page to start again";
-  document.body.style.border = "5px solid red";
-  btnOne.textContent = "Game Over";
-  btnTwo.textContent = "Game Over";
-  div.style.backgroundImage = `url(${url2})`;
-});
+//First room, first victim
+
+const firstVictim = function () {
+  imgTxtChange(
+    newimg,
+    "You go to your fist hunting, you enter a hotel and you see a man you are searching for coming towards you with axe, you will?",
+    "red",
+    "45px"
+  );
+  btnTwo.classList.remove("noneDisplay");
+  btnOne.innerText = "Fight";
+  btnTwo.innerText = "Run";
+  btnOne.addEventListener("click", firstFight);
+  btnTwo.addEventListener("click", choiceRun);
+};
+
+//First choice, fight
+
+const firstFight = function () {
+  document.body.style.backgroundImage = "none";
+  document.body.style.backgroundColor = "red";
+  imgTxtChange(
+    url4,
+    "You choose to fight. Now you roll die, if you have less than 13 you lose, if you have more than 13 you win. If you have 13 you win, and you get 10000 $",
+    "black"
+  );
+  btnOne.textContent = "Roll die";
+  btnTwo.classList.add("noneDisplay");
+  btnOne.addEventListener("click", firstFightRollDie);
+};
+
+//First choice - fight,  roll die
+
+let randomRoll = rollDie();
+
+const firstFightRollDie = function () {
+  btnOne.textContent = "Next";
+  imgTxtChange(gamble, `You got ${randomRoll}`, "blue", "70px");
+  btnOne.addEventListener("click", howMuchWasRollDie);
+  document.body.style.backgroundImage = `url(https://wallpaperaccess.com/full/3703011.jpg)`;
+};
+
+//If die was more than 13, less or equal
+
+const howMuchWasRollDie = function () {
+  btnOne.classList.add("noneDisplay");
+  if (randomRoll > 13) {
+    imgTxtChange(
+      url,
+      "You go outside and live happy ever after",
+      "silver",
+      "55px"
+    );
+  } else if (randomRoll < 13) {
+    imgTxtChange(funeral, "Goodbye");
+    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundColor = "black";
+  } else {
+    imgTxtChange(
+      url5,
+      "You found 10000$ near body, you are ritch bitch now, you bought yourself new bike, and you live life of your dreamns",
+      "black"
+    );
+  }
+};
+
+//Mage second choice, run
+
+const choiceRun = function () {
+  imgTxtChange(
+    url3,
+    "You choose to run like a pussy, you managed to escape and hide in a trash bin near you. Now you are all dirty and smell like shit. After few minutes you exit the trash bin and look around you, it looks clear, but after few steps you feel cold gun on the back of your head. You turn around and you see man that you runned away from.",
+    "blue",
+    "30px"
+  );
+  document.body.style.backgroundImage = "none";
+  document.body.style.backgroundColor = "green";
+  btnTwo.addEventListener("click", secondChoiceRun);
+  btnOne.addEventListener("click", firstFight);
+};
+
+const secondChoiceRun = function () {
+  btnOne.classList.add("noneDisplay");
+  btnTwo.classList.add("noneDisplay");
+  imgTxtChange(
+    url9,
+    "You run but you can't run forever man, this time you did it, but you have to FIGHT TO SURVIVE",
+    "red"
+  );
+};
